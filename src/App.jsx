f5db5063e84cb71ecf88b6f85fc2789c6b964fc4
cajjucajuina <a.beatriizalencar07@gmail.com>
2025-05-14 23:25:54 -0300

    html tranferido para react e pos isso verifiquei o css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react';
 
-function App() {
+function App() { 
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+   
+        <header >
+            <h1>Bem-vindo</h1>
+            <p className="subtitle">entre com suas crendenciais para acessar</p>
+        </header>
+        <form action="/login" method="POST">
+            <div className="form-group">
+                <label for="email">email </label>
+                <div className="input-group">
+                    <input type="email" id="email" name="email" placeholder=" seu email ou nome de usuario" required
+                        autofocus/>
+                </div>
+            </div>
+            <div className="form-group">
+                <label for="password">Senha</label>
+                <div className="input-group">
+                    <input type="password" id="password" name="password" placeholder="senha" required/>
+
+
+                </div>
+
+            </div>
+            <div className="forgot-password">
+                <a href="/recuperar-senha">esqueceu sua senha?</a>
+            </div>
+
+            <div className="options">
+                <div className="remember-me">
+                    <input type="checkbox" id="remember" name="remember"/>
+                    <label for="remember">Lembrar-se</label>
+                </div>
+            </div>
+            <button type="submit">Entrar</button>
+
+        </form>
+        <div className="divider">
+            <span>Ou continue com </span>
+        </div>
+        
+            <a href="/cadastro">Criar conta</a>
+        
+  
+    </> )
+  }
 
 export default App
